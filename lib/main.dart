0587dc338c7da1282c664b53bf6464e83b2633e9
Dhrisya P N <128608814+dhrisyapn@users.noreply.github.com>
2024-08-20 20:20:40 +0530

    add background color

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        scaffoldBackgroundColor: Color(0xff87CEFA),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
